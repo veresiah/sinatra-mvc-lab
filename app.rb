c3ef4739f769835cdd09693059df2695e88473a0
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-01-26 17:20:00 -0500

    commit tree 2b0ae3417119214ebd3beb01957ed2bdb3e24f31

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+    get '/' do 
+        erb :user_input
+    end 
+
+    post '/piglatinize' do 
+        @output = PigLatinizer.new.piglatinize(params[:user_phrase])
+        erb :results
+    end 
 end
